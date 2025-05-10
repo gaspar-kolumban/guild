db02c422b4537972ce84269c2066a228cf4c4a05
Gaspar Kolumban <gaspar.kolumban@gmail.com>
2025-05-10 15:42:11 +0200

    Make a better yaml example in pyyaml test

--- tests/pyyaml_test.py
+++ tests/pyyaml_test.py
@@ -4,14 +4,26 @@ import unittest
 import yaml
 
 
-simple_build = """
-name: HelloWorld
-type: c_library
-interfaces:
-  - helloworld.h
-sources:
-  - main.c
-  - helloworld.c
+simple_example = """
+- type: c_library
+  name: PrintHelloWorldLib
+  interfaces:
+    - helloworld.h
+  sources:
+    - helloworld.c
+
+- type: c_library
+  name: MainLib
+  dependencies:
+    - PrintHelloWorldLib
+  sources:
+    - main.c
+
+- type: c_executable
+  name: Main
+  dependencies:
+    - MainLib
+    - PrintHelloWorldLib
 """
 
 
@@ -20,14 +32,39 @@ class TestYamlFormat(unittest.TestCase):
         with tempfile.TemporaryDirectory() as tmp_dir:
             yaml_file = pathlib.Path(tmp_dir) / "build.yaml"
 
-            with open(yaml_file, "w") as f:
-                f.write(simple_build)
+            with open(yaml_file, "w") as fd:
+                fd.write(simple_example)
 
-            with open(yaml_file, "r") as file:
-                data = yaml.safe_load(file)
+            with open(yaml_file, "r") as fd:
+                data = yaml.safe_load(fd)
 
-            self.assertEqual(data["name"], "HelloWorld")
-            self.assertEqual(data["type"], "c_library")
-            self.assertIn("helloworld.h", data["interfaces"])
-            self.assertIn("helloworld.c", data["sources"])
-            self.assertIn("main.c", data["sources"])
+            self.assertIsInstance(data, list)
+            self.assertEqual(len(data), 3)
+
+            element = data[0]
+            self.assertEqual(element["name"], "PrintHelloWorldLib")
+            self.assertEqual(element["type"], "c_library")
+            self.assertIsInstance(element["interfaces"], list)
+            self.assertEqual(len(element["interfaces"]), 1)
+            self.assertIn("helloworld.h", element["interfaces"])
+            self.assertIsInstance(element["sources"], list)
+            self.assertEqual(len(element["sources"]), 1)
+            self.assertIn("helloworld.c", element["sources"])
+
+            element = data[1]
+            self.assertEqual(element["name"], "MainLib")
+            self.assertEqual(element["type"], "c_library")
+            self.assertIsInstance(element["dependencies"], list)
+            self.assertEqual(len(element["dependencies"]), 1)
+            self.assertIn("PrintHelloWorldLib", element["dependencies"])
+            self.assertIsInstance(element["sources"], list)
+            self.assertEqual(len(element["sources"]), 1)
+            self.assertIn("main.c", element["sources"])
+
+            element = data[2]
+            self.assertEqual(element["name"], "Main")
+            self.assertEqual(element["type"], "c_executable")
+            self.assertIsInstance(element["dependencies"], list)
+            self.assertEqual(len(element["dependencies"]), 2)
+            self.assertIn("MainLib", element["dependencies"])
+            self.assertIn("PrintHelloWorldLib", element["dependencies"])
