8737fc724324366985d3529db54353de76ae229f
Gaspar Kolumban <gaspar.kolumban@gmail.com>
2025-05-10 15:45:43 +0200

    Move run_all_tests.sh to ci folder

--- ci/run_all_tests.sh
+++ ci/run_all_tests.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+# Script is meant to be run from root of repo where the requirements file exists
 REQ_FILE="requirements.txt"
 TEMP_DIR=$(mktemp --directory) 
 echo Using directory: $TEMP_DIR
